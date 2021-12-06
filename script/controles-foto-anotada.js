43db283a5d9b32bd91460188381044dcc319296f
MarceloFCandido <fcandido.marcelo@gmail.com>
2021-12-06 18:58:16 -0300

    doing challenge

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -1,3 +1,18 @@
+function readImage(img, file) {
+    // Check if the file is an image.
+    if (file.type && !file.type.startsWith('image/')) {
+        console.log('File is not an image.', file.type, file);
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+        img.src = event.target.result;
+    });
+
+    reader.readAsDataURL(file);
+}
+
 document
     .getElementById("filtro-da-foto")
     .addEventListener("change", function (e) {
@@ -5,4 +20,13 @@ document
         let rule = children.filter((element) => element.tagName.toLowerCase() === "img")[0]
 
         rule.style.filter = e.target.value
+    })
+
+document
+    .getElementById("imagem")
+    .addEventListener("change", function (e) {
+        let children = Array.from(document.getElementsByClassName("foto-anotada")[0].children)
+        let image = children.filter((element) => element.tagName.toLowerCase() === "img")[0]
+
+        readImage(image, e.target.files[0])
     })
